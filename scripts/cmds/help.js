fbff42e156059cc53afbea246128e1c6df220ac5
─꯭─⃝͎̽𓆩𝑫𝑬𝑽𝑰𝑳 𝑭𝑨𝑹'𝑯𝑨𝑵‣᭄𓆪___//😈🩵🪽 <uffxan576@gmail.com>
2026-05-09 08:24:29 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -57,7 +57,7 @@ module.exports = {
     name: "help",
     aliases: ["menu"],
     version: "2.0",
-    author: "Saimx69x | fixed by Aphelion",
+    author: "MR_FARHAN",
     role: 0,
     category: "info",
     shortDescription: "Show all commands",
